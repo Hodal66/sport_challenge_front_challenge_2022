677462a6da482e7865de249dbf942b8253d26561
Hodal Muheto <mhthodol@gmail.com>
2022-09-19 01:28:06 +0200

    dend

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import MainHeader from "../../components/MainHeader";
 import Programs from "../../components/Programs";
+import Values from "../../components/Values";
 import "./home.css";
 function Home() {
   return (
     <div>
       <MainHeader />
       <Programs />
+      <Values />
     </div>
   );
 }
